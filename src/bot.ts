0155c2947cf3e6878b142c036ee7be6be5deaeb5
Octavio Luperón <armando2445@outlook.com>
2023-10-31 14:58:06 -0400

    Now if a meme type "hola" it will join the voice channel and reproduce an .mp3

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,24 @@
 
-import {Client, GatewayIntentBits, IntentsBitField} from 'discord.js';
+import {Client, GatewayIntentBits, IntentsBitField, Message} from 'discord.js';
+import { joinVoiceChannel, createAudioPlayer, createAudioResource } from '@discordjs/voice';
 import { config } from 'dotenv';
 config();
+
+type bensonInteractionType = {
+    imgPath: string
+    message: string
+}
+
+const bensonInteraction: bensonInteractionType[] = [
+    {
+        imgPath: "./assets/images/benson.png",
+        message: '¿Como \'ta muchacho?'
+    },
+    {
+        imgPath: "./assets/images/muchacho.png",
+        message: 'Yo te veo muy bien'
+    }
+]
 
 const myIntents = new IntentsBitField();
 myIntents.add(
@@ -11,17 +28,54 @@ myIntents.add(
     GatewayIntentBits.GuildMembers
 )
 
-const client: Client = new Client( {intents: myIntents} );
+const client: Client = new Client( {intents: 3276799} );
 
 client.on('ready', () => {
     console.log(`I'm ready. My name is ${client.user?.tag}`)
 })
 
-client.on('messageCreate', (msg) =>{
-    if(msg.content.toLowerCase() === 'hola'){
-        msg.reply('¿Cómo \'tas muchacho?')
-    }
+client.on('messageCreate', async (msg: Message) =>{
+    if(msg.author.bot) return;
 
+    switch(msg.content.toLowerCase()){
+        case 'hola':
+            let random: number = Math.trunc(Math.random() * bensonInteraction.length);
+
+            msg.reply({
+                content: bensonInteraction[random].message,
+                files: [bensonInteraction[random].imgPath]
+            })
+            
+            if (msg.member && msg.member.voice.channel){
+                const voiceChannel = msg.member.voice.channel;
+                if(!msg.guild) return
+
+                const connection = joinVoiceChannel({
+                    channelId: voiceChannel.id,
+                    guildId: msg.guild.id,
+                    adapterCreator: msg.guild?.voiceAdapterCreator,
+                });
+
+                const player = createAudioPlayer();
+                const resource = createAudioResource('./assets/audio/cmtm.mp3')
+                player.play(resource);
+
+                connection.subscribe(player);
+
+                player.on('error', error => {
+                  console.error(`Error: ${error.message} with resource `);
+                });
+            }
+
+            break;
+
+        case 'vete':
+            
+            
+            break;
+
+    }
 })
+
 
 client.login(process.env.TOKEN)
